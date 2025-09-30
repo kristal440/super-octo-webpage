dc99ce94ccf711140fe0aea2b262e7177f883918
zigecXD <ziga.g07@gmail.com>
2025-09-30 08:31:52 +0200

    Add files via upload

--- NalogaSMV/src/config/database.php
+++ NalogaSMV/src/config/database.php
@@ -2,12 +2,13 @@
 // filepath: admin-dashboard/src/config/database.php
 
 $host = 'localhost';
-$dbname = 'admin_dashboard';
-$username = 'root';
-$password = '';
+$db = 'mydb';
+$user = 'myuser';
+$password = 'mypassword';
+$port = '5432'; 
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$db", $user, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
