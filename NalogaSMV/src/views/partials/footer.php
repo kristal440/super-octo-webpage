e1421a4f775439607d680af11c87d93d1e8ff719
Žiga Krašovec <zigakrasovec07@proton.me>
2025-09-30 08:45:12 +0200

    Delete NalogaSMV directory

--- NalogaSMV/src/views/partials/footer.php
+++
@@ -1,9 +0,0 @@
-<?php
-// filepath: /admin-dashboard/admin-dashboard/src/views/partials/footer.php
-?>
-
-<footer>
-    <div class="container">
-        <p>&copy; <?php echo date("Y"); ?> Šolski sistem na daljavo. Vse pravice pridržane.</p>
-    </div>
-</footer>
