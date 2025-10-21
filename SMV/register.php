e1fc1a9f42773043ccbb045ef760df27303abaec
zigecXD <ziga.g07@gmail.com>
2025-10-21 10:44:30 +0200

    Update register.php

--- SMV/register.php
+++ SMV/register.php
@@ -49,7 +49,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 </div>
                 <h2>Šolski sistem na daljavo</h2>
                 <div class="tabs">
-                    <button class="tab" data-tab="register">Registracija</button>
+                    <button class="tab" data-tab="login">Registracija</button>
                 </div>
 
                 <?php if (!empty($error)) echo "<p style='color:red;'>$error</p>"; ?>
@@ -78,6 +78,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     </div>
 </body>
 </html>
+
 
 
 
